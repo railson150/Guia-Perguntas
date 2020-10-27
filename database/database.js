c63981fc2ae3a244ab9d6b4c41b97e3100bcd188
Railson Lopes <43351248+railson150@users.noreply.github.com>
2020-10-27 09:23:43 -0300

    Retirando minha senha do database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
+cont senha = ''
 
-const connection = new Sequelize('guiaperguntas', 'root', 'railson150', {
+const connection = new Sequelize('guiaperguntas', 'root', senha, {
     host: 'localhost',
     dialect: 'mysql'
 })
